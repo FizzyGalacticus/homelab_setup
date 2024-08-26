b71b1d5b215494183310a3f472de15960890d037
Dustin L. Dodson <dustinleedodson@gmail.com>
2024-08-25 20:57:11 -0700

    fix user create

--- dockerfiles/multi_service_postgres/create_user.sh
+++ dockerfiles/multi_service_postgres/create_user.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 set -e
-set -u
 
 function create_user_and_database() {
 	local database=$1
-	echo "  Creating user and database '$database'"
+    local sql="CREATE USER $database WITH PASSWORD '$database'; CREATE DATABASE $database; GRANT ALL PRIVILEGES ON DATABASE $database TO $database; ALTER DATABASE $database OWNER TO $database;"
+	
+    echo "  Creating user and database '$database'"
+
 	psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-	    CREATE USER $database;
-	    CREATE DATABASE $database;
-	    GRANT ALL PRIVILEGES ON DATABASE $database TO $database;
+	    $sql
 EOSQL
 }
 
